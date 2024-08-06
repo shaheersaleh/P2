d74e14c4c2f6d37b1416918d226c6a66ae68f0bb
Shaheer <shaheer.ch143@gmail.com>
2024-08-06 23:12:07 +0500

    done

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 // script.js
-document.addEventListener("DOMContentLoaded", function() {
-    const navbar = document.getElementById('navbar');
-
-    window.addEventListener('scroll', function() {
-        if (window.scrollY > 50) {
-            navbar.classList.add('scrolled');
-        } else {
-            navbar.classList.remove('scrolled');
-        }
-    });
+window.addEventListener('scroll', function() {
+    var navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
 });
